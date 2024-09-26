2e96b1cb3ca93555e489f3e1651799e6bef07232
Palsonroy <Palsonroy123@gmail.com>
2024-09-26 16:48:48 +0530

    prod

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -22,15 +22,15 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
-  default = ["10.2.1.0/24", "10.2.2.0/24"]
+  default = ["10.2.3.0/24", "10.2.4.0/24"]
 }
 
 variable "private_subnets_cidr" {
-  default = ["10.2.11.0/24", "10.2.12.0/24"]
+  default = ["10.2.5.0/24", "10.2.6.0/24"]
 }
 
 variable "database_subnets_cidr" {
-  default = ["10.2.21.0/24", "10.2.22.0/24"]
+  default = ["10.2.7.0/24", "10.2.8.0/24"]
 }
 
 variable "is_peering_required" {
